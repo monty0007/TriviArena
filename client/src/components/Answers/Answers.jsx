f2fad1d0371c1997ceb352549b5c9d5b37bae215
monty0007 <monty.my1234@gmail.com>
2024-09-05 11:59:13 +0530

    addedAnswerList

--- client/src/components/Answers/Answers.jsx
+++ client/src/components/Answers/Answers.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Question } from '../../context/QuestionContext';
+// import './Answers.css';
 
 function Answers() {
+  const { mainQuestion } = useContext(Question);
+  console.log(mainQuestion);
+
   return (
-    <div>Answers</div>
-  )
+    <div className="answers-container">
+      {mainQuestion.map((question, questionIndex) => (
+        <div key={questionIndex} className="question-block">
+          <h3>Question {questionIndex + 1}: {question.question}</h3>
+          <ul className="answer-list">
+            {question.answerList.map((answer, answerIndex) => (
+              <li key={answerIndex} className={`answer-item ${answer.isCorrect ? 'correct-answer' : ''}`}>
+                {answer.body} {answer.isCorrect && <span>(Correct)</span>}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Answers
+export default Answers;
